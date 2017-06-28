4693f92571458995dd507d6c72b2fa7a1c90a747
Jonathan Foong <jonathanfoong1997@gmail.com>
2017-06-28 14:30:35 +0800

    Update UserScene.java

--- JSCinema/src/UserScene.java
+++ JSCinema/src/UserScene.java
@@ -48,7 +48,7 @@ import javafx.stage.Stage;
 			//background pane
 			Pane backgroundPane = new Pane();
 				
-				FileInputStream imageStream = new FileInputStream("Background.png");
+				FileInputStream imageStream = new FileInputStream("UserPage/Background.png");
 				backgroundPane.setPadding(new Insets(0,0,0,0));
 				Image image = new Image(imageStream);
 				backgroundPane.getChildren().add(new ImageView(image));
@@ -56,7 +56,7 @@ import javafx.stage.Stage;
 			//footer pane
 			HBox Footer = new HBox();
 			
-				FileInputStream imageStream2 = new FileInputStream("Logo.png");
+				FileInputStream imageStream2 = new FileInputStream("UserPage/Logo.png");
 				Footer.setPadding(new Insets(30,0,0,530));
 				Image image2 = new Image(imageStream2);
 				Footer.getChildren().add(new ImageView(image2));
@@ -64,7 +64,7 @@ import javafx.stage.Stage;
 			//menu bar background pane
 			HBox banner = new HBox();
 				
-				FileInputStream imageStream3 = new FileInputStream("banner.png");
+				FileInputStream imageStream3 = new FileInputStream("UserPage/banner.png");
 				banner.setPadding(new Insets(125,0,0,30));
 				Image image3 = new Image(imageStream3);
 				ImageView Img = new ImageView(image3);
@@ -94,13 +94,14 @@ import javafx.stage.Stage;
 				
 			//movies pane
 			HBox MoviesImage = new HBox(0);
-			MoviesImage.setStyle("-fx-background: transparent");
-			MoviesImage.setStyle("-fx-background-color: transparent");
-			java.io.File file = new java.io.File("movies.txt");
-			Scanner input = new Scanner(file);
-			ImageView [] movieImgView = new ImageView[10];
-			int count =0;
-			while(input.hasNext()){
+			
+				MoviesImage.setStyle("-fx-background: transparent");
+				MoviesImage.setStyle("-fx-background-color: transparent");
+				java.io.File file = new java.io.File("UserPage/movies.txt");
+				Scanner input = new Scanner(file);
+				ImageView [] movieImgView = new ImageView[10];
+				int count =0;
+				while(input.hasNext()){
 				String line = input.nextLine();
 				String[] splitter = line.split(",");
 				FileInputStream movieStream = new FileInputStream(splitter[12]);
@@ -115,12 +116,13 @@ import javafx.stage.Stage;
 			
 			//moviesLbl
 			HBox MovieTitle = new HBox(0);
-			MovieTitle.setStyle("-fx-background: transparent");
-			MovieTitle.setStyle("-fx-background-color: transparent");
-			Scanner inputTitle = new Scanner(file);
-			Button [] movieBtn = new Button[10];
-			int count2 =0;
-			while(inputTitle.hasNext()){
+			
+				MovieTitle.setStyle("-fx-background: transparent");
+				MovieTitle.setStyle("-fx-background-color: transparent");
+				Scanner inputTitle = new Scanner(file);
+				Button [] movieBtn = new Button[10];
+				int count2 =0;
+				while(inputTitle.hasNext()){
 				String line = inputTitle.nextLine();
 				String[] splitter = line.split(",");
 
@@ -132,22 +134,25 @@ import javafx.stage.Stage;
 				
 				
 				count2 ++;
-			}
-			inputTitle.close();
+				}
+				inputTitle.close();
 			
+			//VBox to fit image and button of each movie
 			VBox MoviesPane = new VBox(0);
-			MoviesPane.setStyle("-fx-background: transparent");
-			MoviesPane.setStyle("-fx-background-color: transparent");
-			MoviesPane.getChildren().addAll(MoviesImage,MovieTitle);
+			
+				MoviesPane.setStyle("-fx-background: transparent");
+				MoviesPane.setStyle("-fx-background-color: transparent");
+				MoviesPane.getChildren().addAll(MoviesImage,MovieTitle);
 			
 			//scroll through movies pane
 			ScrollPane sp1 = new ScrollPane();
-			sp1.setContent(MoviesPane);
-			sp1.setStyle("-fx-background: transparent");
-			sp1.setStyle("-fx-background-color: transparent");
-			sp1.setHbarPolicy(ScrollBarPolicy.ALWAYS);
-			sp1.setVbarPolicy(ScrollBarPolicy.NEVER);
-			sp1.addEventFilter(ScrollEvent.SCROLL,new EventHandler<ScrollEvent>() {
+			
+				sp1.setContent(MoviesPane);
+				sp1.setStyle("-fx-background: transparent");
+				sp1.setStyle("-fx-background-color: transparent");
+				sp1.setHbarPolicy(ScrollBarPolicy.ALWAYS);
+				sp1.setVbarPolicy(ScrollBarPolicy.NEVER);
+				sp1.addEventFilter(ScrollEvent.SCROLL,new EventHandler<ScrollEvent>() {
 				@Override
 				public void handle(ScrollEvent event) {
 					if (event.getDeltaY() != 0)
@@ -155,20 +160,22 @@ import javafx.stage.Stage;
 						event.consume();
 					}
 				}
-	    });
-		sp1.setMaxSize(1370, 750);
+				});
+				sp1.setMaxSize(1370, 750);
 		
 		//pane to fit more pane :0(scroll Pane)
 		HBox hbox1 = new HBox();
-		hbox1.setStyle("-fx-background: transparent");
-		hbox1.setStyle("-fx-background-color: transparent");
+		
+			hbox1.setStyle("-fx-background: transparent");
+			hbox1.setStyle("-fx-background-color: transparent");
 			hbox1.setPadding(new Insets(200,0,0,30));
 			hbox1.getChildren().add(sp1);
 		
 		//
 		HBox border = new HBox();
-		border.setPadding(new Insets(600,0,0,18));
-			FileInputStream imageStream7 = new FileInputStream("border.png");
+		
+			border.setPadding(new Insets(600,0,0,18));
+			FileInputStream imageStream7 = new FileInputStream("UserPage/border.png");
 			Image blueBorder = new Image(imageStream7);
 			ImageView Bord1 = new ImageView(blueBorder);
 			Bord1.setFitHeight(220);
@@ -213,7 +220,7 @@ import javafx.stage.Stage;
 				ShowingTimeAvailable.setFont(Font.font("Times New Roman",FontWeight.BOLD, 25));
 				ShowingTimeAvailable.setTextFill(Color.AQUA);
 			
-			Label Movie_Name = new Label("Cinema Name");
+				Label Movie_Name = new Label("Cinema Name");
 				Movie_Name.setFont(Font.font("Times New Roman", 22));
 				Movie_Name.setTextFill(Color.AQUA);
 				
@@ -227,14 +234,15 @@ import javafx.stage.Stage;
 			
 			//Proceed btn Pane
 			HBox BtnPane = new HBox(20);
-			BtnPane.setPadding(new Insets(830,0,0,980));
-			Button Back = new Button("Back");
-			Button Proceed = new Button("Proceed");
-			Back.setMinWidth(200);
-			Back.getStyleClass().add("button2");
-			Proceed.setMinWidth(200);
-			Proceed.getStyleClass().add("button2");
-			BtnPane.getChildren().addAll(Back,Proceed);
+			
+				BtnPane.setPadding(new Insets(830,0,0,980));
+				Button Back = new Button("Back");
+				Button Proceed = new Button("Proceed");
+				Back.setMinWidth(200);
+				Back.getStyleClass().add("button2");
+				Proceed.setMinWidth(200);
+				Proceed.getStyleClass().add("button2");
+				BtnPane.getChildren().addAll(Back,Proceed);
 			
 			if(no_Movies >= 1){
 				movieBtn[0].addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
@@ -250,7 +258,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[0]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -291,7 +299,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[1]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -332,7 +340,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[2]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -373,7 +381,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[3]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -414,7 +422,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[4]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -455,7 +463,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[5]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -496,7 +504,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[6]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -537,7 +545,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[7]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -578,7 +586,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[8]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -619,7 +627,7 @@ import javafx.stage.Stage;
 		            	
 		            	Movie_Name.setText(movieNames[9]);
 		            	try{
-		            	java.io.File file = new java.io.File("movies.txt");
+		            	java.io.File file = new java.io.File("UserPage/movies.txt");
 		            	Scanner input = new Scanner(file);
 		            	while(input.hasNext()){
 		            		String line = input.nextLine();
@@ -659,6 +667,7 @@ import javafx.stage.Stage;
 			
 			rootPane.getChildren().addAll(backgroundPane,Footer,banner,border,BtnPane,showingTime,hbox1,MenuBar);
 			Scene scene = new Scene(rootPane, 1440 , 960 );
+			scene.getStylesheets().add("style.css");
 			stage.setTitle("JSC");
 			return scene;
 		}
@@ -670,7 +679,7 @@ import javafx.stage.Stage;
 			
 			Pane backgroundPane = new Pane();
 			
-			FileInputStream imageStream = new FileInputStream("Background.png");
+			FileInputStream imageStream = new FileInputStream("UserPage/Background.png");
 			backgroundPane.setPadding(new Insets(0,0,0,0));
 			Image image = new Image(imageStream);
 			backgroundPane.getChildren().add(new ImageView(image));
@@ -678,7 +687,7 @@ import javafx.stage.Stage;
 		//footer pane[scene2]
 		HBox Footer = new HBox();
 		
-			FileInputStream imageStream2 = new FileInputStream("Logo.png");
+			FileInputStream imageStream2 = new FileInputStream("UserPage/Logo.png");
 			Footer.setPadding(new Insets(30,0,0,530));
 			Image image2 = new Image(imageStream2);
 			Footer.getChildren().add(new ImageView(image2));
@@ -687,7 +696,7 @@ import javafx.stage.Stage;
 		HBox banner2 = new HBox();
 		
 			banner2.setPadding(new Insets(125,0,0,30));
-			FileInputStream imageStream11 = new FileInputStream("banner.png");
+			FileInputStream imageStream11 = new FileInputStream("UserPage/banner.png");
 			Image image11 = new Image(imageStream11);
 			ImageView Img1 = new ImageView(image11);
 			Img1.setFitHeight(60);
@@ -716,18 +725,18 @@ import javafx.stage.Stage;
 			
 		HBox BorderClassPane = new HBox();
 			
-			BorderClassPane.setPadding(new Insets(200,0,0,100));
-			FileInputStream imageStream12 = new FileInputStream("SceneBorder2.png");
+			BorderClassPane.setPadding(new Insets(200,0,0,50));
+			FileInputStream imageStream12 = new FileInputStream("UserPage/SceneBorder2.png");
 			Image image12 = new Image(imageStream12);
 			ImageView Img12 = new ImageView(image12);
-			Img12.setFitHeight(600);
-			Img12.setFitWidth(1200);
+			Img12.setFitHeight(750);
+			Img12.setFitWidth(700);
 			BorderClassPane.getChildren().add(Img12);
 			
 		VBox NowShowingMov = new VBox(20);
-			
-			NowShowingMov.setPadding(new Insets(350,0,0,180));
-			java.io.File file = new java.io.File("movies.txt");
+		
+			NowShowingMov.setPadding(new Insets(350,0,0,100));
+			java.io.File file = new java.io.File("UserPage/movies.txt");
 			Scanner input = new Scanner(file);
 			Scanner MovName = new Scanner(file);
 			Label movieNames [] = new Label [10];
@@ -744,12 +753,12 @@ import javafx.stage.Stage;
 				
 			}
 			MovName.close();
-			
+	
 		VBox CommingSoonMov = new VBox(20);
 		
-			
-			//CommingSoonMov.setPadding(new Insets(350,0,0,360));
-			FileInputStream file2 = new FileInputStream("comingSoon.txt");
+	
+			CommingSoonMov.setPadding(new Insets(350,0,0,360));
+			FileInputStream file2 = new FileInputStream("UserPage/comingSoon.txt");
 			Scanner CmgName = new Scanner(file2);
 			Label comingSoonNames [] = new Label [10];
 			int count4 =0;
@@ -757,14 +766,14 @@ import javafx.stage.Stage;
 				String line = CmgName.nextLine();
 				String[] splitter = line.split(",");
 
-				comingSoonNames[count4] = new Label((count4+1)+"."+splitter[0]);
+				comingSoonNames[count4] = new Label((count4+1)+"."+splitter[0]+"         released on: "+splitter[1]);
 				comingSoonNames[count4].setFont(Font.font("Arial Black", 20));
 				comingSoonNames[count4].setTextFill(Color.TOMATO);
 				NowShowingMov.getChildren().add(comingSoonNames[count4]);
 				count4 ++;
 			}
 			CmgName.close();
-			
+	
 				
 				rootPane2.getChildren().addAll(backgroundPane,Footer,BorderClassPane,NowShowingMov,CommingSoonMov,banner2,MenuBar2);
 				Scene scene = new Scene(rootPane2, 1440 , 960 );
