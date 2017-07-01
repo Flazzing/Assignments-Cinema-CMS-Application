310b7abef6b9e6f6d549499a4f87d8f2449d2109
Flazzing <limchengqing@yahoo.com>
2017-07-01 21:06:43 +0800

    Add files via upload

--- JSCinema/src/ComingSoonMovie.java
+++ JSCinema/src/ComingSoonMovie.java
@@ -1,5 +1,17 @@
 
 public class ComingSoonMovie {
+
+
+	public ComingSoonMovie(String movieName, String releaseDay, String releaseMonth, String movieDescription,
+			String movieImageDirectory) {
+		super();
+		MovieName = movieName;
+		ReleaseDay = releaseDay;
+		ReleaseMonth = releaseMonth;
+		MovieDescription = movieDescription;
+		MovieImageDirectory = movieImageDirectory;
+	}
+	private String MovieName= null;
 	public String getMovieName() {
 		return MovieName;
 	}
@@ -18,19 +30,27 @@ public class ComingSoonMovie {
 	public void setReleaseMonth(String releaseMonth) {
 		ReleaseMonth = releaseMonth;
 	}
-	public ComingSoonMovie(String movieName, String releaseDay, String releaseMonth) {
-		super();
-		MovieName = movieName;
-		ReleaseDay = releaseDay;
-		ReleaseMonth = releaseMonth;
+	public String getMovieDescription() {
+		return MovieDescription;
+	}
+	public void setMovieDescription(String movieDescription) {
+		MovieDescription = movieDescription;
+	}
+	public String getMovieImageDirectory() {
+		return MovieImageDirectory;
+	}
+	public void setMovieImageDirectory(String movieImageDirectory) {
+		MovieImageDirectory = movieImageDirectory;
 	}
 	@Override
 	public String toString() {
 		return "ComingSoonMovie [MovieName=" + MovieName + ", ReleaseDay=" + ReleaseDay + ", ReleaseMonth="
-				+ ReleaseMonth + "]";
+				+ ReleaseMonth + ", MovieDescription=" + MovieDescription + ", MovieImageDirectory="
+				+ MovieImageDirectory + "]";
 	}
-	private String MovieName= null;
 	private String ReleaseDay= null;
 	private String ReleaseMonth = null;
+	private String MovieDescription = null;
+	private String MovieImageDirectory = null;
 }
  
